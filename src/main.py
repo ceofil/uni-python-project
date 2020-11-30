417894ff07fc0c60f46446da15da7ad8308209d9
ceofil <gceofil@gmail.com>
2020-11-30 15:54:53 +0200

    line.py (no thickness)

--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
 from src.canvas import Canvas
 from src.shapes.rect import Rect
 from src.shapes.circle import Circle
+from src.shapes.line import Line
 
 
 def main():
     canvas = Canvas(255, 255)
-    rect = Rect(100, 50, 50, 100, (255, 255, 0), (255, 0, 255), 6)
-    rect.draw(canvas)
+    Rect(100, 50, 50, 100, (255, 255, 0), (255, 0, 255), 6).draw(canvas)
 
-    circle = Circle(50, 50, 20, (255, 255, 0), (255, 0, 255), 6)
-    circle1 = Circle(150, 150, 20, (255, 255, 0), None, 6)
-    circle2 = Circle(150, 130, 20, None, (255, 0, 255), 6)
-    circle.draw(canvas)
-    circle1.draw(canvas)
-    circle2.draw(canvas)
+    Circle(50, 50, 20, (255, 255, 0), (255, 0, 255), 6).draw(canvas)
+    Circle(150, 150, 20, (255, 255, 0), None, 6).draw(canvas)
+    Circle(150, 130, 20, None, (255, 0, 255), 6).draw(canvas)
+
+    Line(50, 125, 60, 200, (255, 255, 255), 1).draw(canvas)
+    Line(65, 200, 150, 220, (255, 255, 255), 1).draw(canvas)
+
     canvas.dump_to_png('test.png')
 
 
---
+++ src/shapes/line.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class Line:
+    def __init__(self, x0, y0, x1, y1, color, thickness=2):
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
+        self.color = color
+        self.thickness = thickness
+
+    def draw(self, canvas):
+        dx = abs(self.x1 - self.x0)
+        dy = abs(self.y1 - self.y0)
+
+        if dy > dx:
+            m = dx / dy
+            b = self.x0 - m * self.y0
+            y_start, y_end = min(self.y0, self.y1), max(self.y0, self.y1)
+            for y in np.arange(y_start, y_end, 1):
+                x = m * y + b
+                canvas.put_pixel(x, y, self.color)
+        else:
+            m = dy / dx
+            b = self.y0 - m * self.x0
+            x_start, x_end = min(self.x0, self.x1), max(self.x0, self.x1)
+
+            for x in np.arange(x_start, x_end, 1):
+                y = m * x + b
+                canvas.put_pixel(x, y, self.color)
